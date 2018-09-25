3962c3d51f60003b0a6c12e4add02aa45e6d8851
Yuka Mullen <quiwah@gmail.com>
2018-09-25 09:29:35 -0700

    Fixed the moveShelf async issue

--- src/App.js
+++ src/App.js
@@ -19,10 +19,10 @@ class BooksApp extends React.Component {
   }
 
   moveShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf);
-
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books: books })
+    BooksAPI.update(book, shelf).then(() => {
+      BooksAPI.getAll().then((books) => {
+        this.setState({ books: books })
+      })
     })
   }
 
